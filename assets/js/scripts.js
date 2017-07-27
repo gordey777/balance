641ff5c85b381820502423b8aac57151065b4e9c
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2017-07-27 11:13:40 +0300

    home and singleready

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,4 +20,67 @@ if (typeof jQuery === 'undefined') {
 } else {
   console.log('jQuery has loaded');
 }
-// Place any jQuery/helper plugins in here.
+
+jQuery(document).ready(function() {
+  //SLIDERS OPTIONS
+  jQuery('.two-slides.owl-carousel').owlCarousel({
+    lazyLoad: true,
+    slideSpeed: 5000,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 0,
+    navText: '',
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      }
+    },
+  });
+  jQuery('.four-slides.owl-carousel').owlCarousel({
+    lazyLoad: true,
+    slideSpeed: 5000,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 50,
+    navText: '',
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      }
+    },
+  });
+
+  (function($) {
+    $(function() {
+
+      $('.tabs .tabs__caption').on('click', '.tab__label:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+
+    });
+  })(jQuery);
+});
